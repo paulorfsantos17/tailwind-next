7f885c2a82a3fd72f29ee118ea2e6bd2668ec6b4
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-25 19:07:12 -0300

    feat: add Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,3 +1,22 @@
+import { Search } from 'lucide-react'
+import { Logo } from './Logo'
+import { MainNavigation } from './MainNavigation'
+
 export function Sidebar() {
-  return <aside className="border-r border-zinc-200 px-5 py-8">sidebar</aside>
+  return (
+    <aside className="space-y-6 border-r border-zinc-200 px-5 py-8">
+      <Logo />
+
+      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
+        <Search className="size-5 text-zinc-500" />
+        <input
+          type="text"
+          placeholder="Search"
+          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+        />
+      </div>
+
+      <MainNavigation />
+    </aside>
+  )
 }
