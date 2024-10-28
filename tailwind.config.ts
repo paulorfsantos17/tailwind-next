39edb41f17480ea9d3fecb8ce6058515de4cf6a6
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-28 15:26:15 -0300

    feat: add variants in button and fileItem

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,19 @@ const config: Config = {
         violet: {
           25: '#fcfaff',
         },
+        error: {
+          25: '#FFFBFA',
+          50: '#FEF3F2',
+          100: '#FEE4E2',
+          200: '#FECDCA',
+          300: '#FDA29B',
+          400: '#F97066',
+          500: '#F04438',
+          600: '#D92D20',
+          700: '#B42318',
+          800: '#912018',
+          900: '#7A271A',
+        },
       },
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
