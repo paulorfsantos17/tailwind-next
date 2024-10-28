a7146c476577af565e270f7b73c7c379380f4932
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-28 15:55:42 -0300

    feat: ensure responsive Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,36 +1,52 @@
-import { Cog, LifeBuoy, Search } from 'lucide-react'
+'use client'
+
+import { Cog, LifeBuoy, Menu, Search } from 'lucide-react'
 import { Logo } from './Logo'
 import { MainNavigation } from './MainNavigation'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { NavItem } from './MainNavigation/NavItem'
 import { Profile } from './Profile'
 import { InputControl, InputPrefix, InputRoot } from '../Form/Input'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Button } from '../Button'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
-      <Logo />
-
-      <InputRoot>
-        <InputPrefix>
-          <Search className="size-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
-
-      <MainNavigation />
-      <div className="mt-auto flex flex-col gap-6">
-        <nav className="space-y-0.5">
-          <NavItem title="Support" icon={LifeBuoy} />
-          <NavItem title="Settings" icon={Cog} />
-        </nav>
-
-        <UsedSpaceWidget />
-
-        <div className="h-px bg-zinc-200" />
-
-        <Profile />
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 data-[state=open]:h-screen lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 lg:data-[state=closed]:h-screen">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger className="lg:hidden" asChild>
+          <Button variant="ghost">
+            <Menu className="size-6" />
+          </Button>
+        </Collapsible.Trigger>
       </div>
-    </aside>
+
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-4 data-[state=closed]:hidden lg:data-[state=closed]:flex"
+      >
+        <InputRoot>
+          <InputPrefix>
+            <Search className="size-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
+
+        <MainNavigation />
+        <div className="mt-auto flex flex-col gap-6">
+          <nav className="space-y-0.5">
+            <NavItem title="Support" icon={LifeBuoy} />
+            <NavItem title="Settings" icon={Cog} />
+          </nav>
+
+          <UsedSpaceWidget />
+
+          <div className="h-px bg-zinc-200" />
+
+          <Profile />
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
